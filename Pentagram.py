937c8489f024df922fb51312c57256b241c94555
daladida <68273945+daladida@users.noreply.github.com>
2021-08-14 21:59:24 +0800

    Add files via upload

---
+++ Pentagram.py
@@ -0,0 +1,13 @@
+#绘制五角星
+from turtle import Turtle
+
+p = Turtle()
+p.speed(3)
+p.pensize(5)
+p.color("black",'yellow')
+#p.fillcolor("red")
+p.begin_fill()
+for i in range(5):
+    p.forward(200)
+    p.right(144)
+p.end_fill()
